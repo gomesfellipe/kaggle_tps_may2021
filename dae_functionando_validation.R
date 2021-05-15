8d78427c2cb2b2f618f6bd269a3475b0d0ebeada
gomesfellipe <gomes.fellipe@hotmail.com>
2021-05-15 18:09:19 -0300

    add materiais

--- dae_functionando_validation.R
+++ dae_functionando_validation.R
@@ -1,4 +1,7 @@
+# knitr::opts_chunk$set(echo = TRUE)
+
 library(tidyverse)
+library(tidymodels)
 library(keras)
 
 
@@ -25,21 +28,16 @@ x_val <-
   mutate_all(scale) %>% 
   as.matrix()
 
-batch_size <- 64
-epochs <- 50
-steps_per_epoch <- 5
-
 # set model
 model <- keras_model_sequential()
 model %>%
-  layer_dense(units = 256, activation = "relu", input_shape = ncol(x_train)) %>%
-  # layer_dense(units = 128, activation = "relu") %>%
-  layer_dense(units = 64, activation = "relu", name = "bottleneck") %>%
-  # layer_dense(units = 128, activation = "relu") %>%
-  layer_dense(units = 256, activation = "relu") %>%
+  layer_dense(units = 128, activation = "relu", input_shape = ncol(x_train)) %>%
+  #layer_dense(units = 512, activation = "relu") %>%
+  layer_dense(units = 32, activation = "relu", name = "bottleneck") %>%
+  #layer_dense(units = 512, activation = "relu") %>%
+  layer_dense(units = 128, activation = "relu") %>%
   layer_dense(units = ncol(x_train), activation = "linear")
 
-# view model layers
 summary(model)
 
 model %>% compile(
@@ -47,6 +45,8 @@ model %>% compile(
   optimizer = optimizer_adam(
     lr = 0.001 ))
 
+
+# Columnwise swap noise 
 batch_generator <- function(data, batch_size, perc=.15)
 {
   function() {
@@ -65,13 +65,36 @@ batch_generator <- function(data, batch_size, perc=.15)
   }
 }
 
+# Fixed parameters
+batch_size <- 128
+epochs <- 100
+
+#batch_generator_val <- function(x_val, batch_size, perc=.15)
+#{
+#  function() {
+x_val_generator <- x_val
+
+ids <- sample(1:nrow(x_val_generator), batch_size, replace = FALSE)
+
+for(j in 1:ncol(x_val_generator)){
+  to_shuffle <- sample(c(T, F), size = nrow(x_val_generator[ids, ]), replace = T, prob = c(.15, 1-.15))
+  x_val_generator[ids,][which(to_shuffle), j] <- sample(x_val_generator[ids,][which(to_shuffle), j])
+}
+#    list(
+#      as.matrix(x_val_generator[ids, ]),
+#      as.matrix(x_val[ids, ])
+#    )
+#  }
+#}
+
 ## This code works
 history <- model %>%
   fit_generator(generator = keras:::as_generator.function(batch_generator(x_train, batch_size)),
-                steps_per_epoch = steps_per_epoch,
+                steps_per_epoch = 100,
                 
-                validation_data = keras:::as_generator.function(batch_generator(x_val, batch_size)),
-                validation_steps = floor(steps_per_epoch/2),
+                #validation_data = keras:::as_generator.function(batch_generator(x_val, batch_size)),
+                validation_steps = 10,
+                validation_data = list(x_val_generator[ids, ], x_val[ids, ]),
                 
                 epochs = epochs, 
                 view_metrics = TRUE, 
@@ -82,6 +105,8 @@ history <- model %>%
                   restore_best_weights = TRUE
                 )),
                 verbose = 2)
+
+# Plot history
 
 as_metrics_df = function(history) {
   
@@ -106,7 +131,7 @@ as_metrics_df(history) %>%
   geom_point()+
   geom_smooth(se = F)+
   theme_bw()
-  
+
 
 mse.ae2 <- evaluate(model, x_train, x_train)
 mse.ae2
@@ -130,6 +155,64 @@ daeta_full <- bind_cols(
 
 daeta_train <- daeta_full %>% filter(id %in% train$id)
 daeta_test <- daeta_full %>% filter(id %in% test$id)
+
+
+titanic_folds <- daeta_train %>% rsample::vfold_cv(v = 4,repeats = 2, strata = target)
+
+titanic_recipe <- recipe(target~., data=daeta_train) %>%
+  step_rm(id) %>%
+  step_zv(all_predictors())
+
+lgbm_spec <- boost_tree(
+  mtry = tune()
+) %>%  
+  set_engine("xgboost") %>% 
+  set_mode("classification")
+
+lgbm_wflow <- workflow() %>% 
+  add_recipe(titanic_recipe) %>% 
+  add_model(lgbm_spec) 
+
+lgbm_params <- 
+  dials::parameters(
+    # learn_rate(),           # learning_rate
+    # trees()                 # num_iterations
+    # min_n(),                  # min_data_in_leaf
+    # tree_depth(c(1, 16)),     # max_depth
+    # sample_prop(c(0.1, 0.9)), # bagging_fraction (to sample_size)
+    mtry()                   # feature_fraction
+    # loss_reduction(),         # min_gain_to_split
+    # reg_lambda = dials::penalty(),
+    # reg_alpha = dials::penalty(),
+    # num_leaves = dials::tree_depth(c(15, 255))
+  )
+
+lgbm_params <- lgbm_params %>% 
+  finalize(daeta_train)
+
+set.seed(314)
+lgbm_res <-
+  lgbm_wflow %>% 
+  tune_bayes(
+    resamples = titanic_folds,
+    param_info = lgbm_params,
+    # initial = 9,
+    iter = 9999,
+    metrics = metric_set(mn_log_loss),
+    control = control_bayes(no_improve = 9999, time_limit=10,
+                            verbose = TRUE, save_pred = FALSE)
+  )
+
+lgbm_res %>%
+  collect_metrics()
+
+autoplot(lgbm_res)
+
+lgbm_final_wflow_tun <- 
+  finalize_workflow(
+    lgbm_wflow,
+    select_best(lgbm_res, metric = 'mn_log_loss') )
+
 
 
 # rascunho ----------------------------------------------------------------
